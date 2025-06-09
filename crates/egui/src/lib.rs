fea36ff31db1b271be5ac2ed302d379aa1d547fa
sssa <313999717@qq.com>
2025-06-09 10:31:35 +0800

    export grid size

--- crates/egui/src/lib.rs
+++ crates/egui/src/lib.rs
@@ -490,7 +490,7 @@ pub use self::{
     },
     drag_and_drop::DragAndDrop,
     epaint::text::TextWrapMode,
-    grid::Grid,grid::GridLayout,grid::State as GridState,
+    grid::Grid,grid::GridLayout,grid::State as GridState,grid::GridSize,
     id::{Id, IdMap},
     input_state::{InputState, MultiTouchInfo, PointerState},
     layers::{LayerId, Order},
